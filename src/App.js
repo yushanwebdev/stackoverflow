3c5052466bcdbfcc252bb40bfe53d267ef774b5e
yushan <yushan@intervest.lk>
2021-05-31 18:32:36 +0530

    feat: Control the visibility of the model by setting a state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [isAddProjectClicked, setisAddProjectClicked] = useState(true);
+  const [addProjectModal, openAddProjectModal] = useState("none");
+
+  const renderAddProject = () => {
+    return (
+      <div>
+        <div className="task-accordion" style={{ display: addProjectModal }}>
+          <i
+            className="fa fa-close close-icon"
+            id="closeIconn"
+            onClick={() => openAddProjectModal("block")}
+            aria-hidden="true"
+          ></i>
+
+          <h4>ADD Project</h4>
+          <div className="form-group">
+            <textarea
+              id="textAreaAddProject"
+              type="text"
+              style={{ height: "30vh" }}
+              placeholder="Project Title"
+              name="Title"
+              className="form-control"
+              required
+            />
+          </div>
+          <div className="col-sm-12 add-mupps-button">
+            <button type="submit">Save</button>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {isAddProjectClicked && renderAddProject()}
+      <button onClick={() => openAddProjectModal("block")}>Click Me</button>
+    </>
   );
 }
 

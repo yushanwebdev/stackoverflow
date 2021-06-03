eb8bd6fc7c838e0a6974e82b23d1a049d67df8af
yushan <yushan@intervest.lk>
2021-06-03 11:10:25 +0530

    feat: Fix the issue

--- src/App.js
+++ src/App.js
@@ -1,24 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Form, FormGroup, Label, Input, Button } from "reactstrap";
 
 function App() {
+  const [selectedUser, setSelectedUser] = useState({
+    id: "",
+    name: "",
+    nameL: "",
+    email: "",
+    contact: "",
+    address: "",
+    date: "",
+  });
+
+  const onChange = (e) => {
+    const property = e.target.name;
+    const value = e.target.value;
+
+    setSelectedUser({
+      ...selectedUser,
+      [property]: value,
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Form>
+        <FormGroup>
+          <Label>FirstName</Label>
+          <Input
+            type="text"
+            name="name"
+            value={selectedUser.name}
+            onChange={onChange}
+            placeholder="enter first name"
+          ></Input>
+          <Label>Last Name:</Label>
+          <Input
+            type="text"
+            name="nameL"
+            value={selectedUser.nameL}
+            onChange={onChange}
+            placeholder="enter your Last name"
+          ></Input>
+          <Label>Email:</Label>
+          <Input
+            type="email"
+            name="email"
+            value={selectedUser.email}
+            onChange={onChange}
+            placeholder="Email Address"
+          ></Input>
+          <Label>Contact Number:</Label>
+          <Input
+            type="number"
+            name="contact"
+            value={selectedUser.contact}
+            onChange={onChange}
+            placeholder="Contact"
+          ></Input>
+          <Label>Address:</Label>
+          <Input
+            type="text"
+            name="address"
+            value={selectedUser.address}
+            onChange={onChange}
+            placeholder="enter your Address"
+          ></Input>
+          <Label>Date</Label>
+          <Input
+            type="date"
+            name="date"
+            value={selectedUser.date}
+            onChange={onChange}
+            placeholder="enter date employed"
+          ></Input>
+        </FormGroup>
+        <Button type="submit" className="btn btn-info">
+          Edit{" "}
+        </Button>
+      </Form>
+    </React.Fragment>
   );
 }
 

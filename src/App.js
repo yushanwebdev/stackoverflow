d24b84d4b978c30619dc37586da01b64e3793dc6
yushan <yushan@intervest.lk>
2021-06-07 16:57:56 +0530

    67869752: Develop the user's requirement

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import DOMPurify from "dompurify";
+import ReactHtmlParser from "react-html-parser";
+import data from "./data.json";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>{data.map((content) => ReactHtmlParser(DOMPurify.sanitize(content)))}</>
   );
 }
 

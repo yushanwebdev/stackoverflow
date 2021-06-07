4b529db07f05da081b5fddee661a49c9bb582c8a
yushan <yushan@intervest.lk>
2021-06-07 15:21:31 +0530

    67868048: Develop the user's requirment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { DateRangePickerComponent } from "@syncfusion/ej2-react-calendars";
 
-function App() {
+import "./App.css";
+
+const App = () => {
+  const [date, updateDate] = useState();
+
+  const onChange = (e) => {
+    updateDate(e.text);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <DateRangePickerComponent change={onChange} />
+      <div>Date - {date}</div>
     </div>
   );
-}
+};
 
 export default App;

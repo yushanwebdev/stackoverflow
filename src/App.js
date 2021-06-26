dac9f2214a6bc5074524552c4549b5ff61403656
Yushan Fernando <yushanwebdev@gmail.com>
2021-06-26 14:11:22 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+const App = ({ actions }) => {
+  const [players, setPlayers] = useState({ player1: 20, player2: 20 });
+  const [count, setCount] = useState(0);
+
+  const handleActions = () => {
+    let processPlayer = players;
+    let finalCount = count;
+
+    actions.forEach((player) => {
+      const count = processPlayer[player.takeFrom] - player.amount;
+      const maxAmount = Math.min(player.amount, processPlayer[player.takeFrom]);
+
+      finalCount += maxAmount;
+      processPlayer[player.takeFrom] = count >= 0 ? count : 0;
+    });
+
+    setPlayers({
+      player1: processPlayer.player1,
+      player2: processPlayer.player2
+    });
+
+    setCount(finalCount);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Count: {count}</p>
+      <ul>
+        {Object.entries(players).map(([playerId, points]) => (
+          <li key={playerId}>
+            {playerId}: {points}
+          </li>
+        ))}
+      </ul>
+      <button onClick={handleActions}>Perform actions</button>
     </div>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App
+      actions={[
+        { takeFrom: "player1", amount: 5 }, // now player1 has 15 points (20 - 5 )✅
+        { takeFrom: "player2", amount: 10 }, // now player 2 has 10 points ✅
+        { takeFrom: "player1", amount: 20 } // now player1 has 0 points (cap the amount) ❌
+      ]}
+    />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
